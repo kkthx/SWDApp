dae8d89d15117c883e606b882465aff19178dbe1
Lukasz <lukasz@kkthx.pl>
2017-06-08 15:59:22 +0200

    initial second activity: criterion select

--- SWDApp/MainActivity.cs
+++ SWDApp/MainActivity.cs
@@ -1,18 +1,37 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+using Android.Content;
+
 
 namespace SWDApp
 {
     [Activity(Label = "SWDApp", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        public static readonly List<string> phoneNumbers = new List<string>();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+           
+            Button b1 = FindViewById<Button>(Resource.Id.button1);
+
+            b1.Click += (object sender, EventArgs e) =>
+            {
+                StartActivity(typeof(CriterionSelectActivity));
+            };
+
+
         }
     }
 }
